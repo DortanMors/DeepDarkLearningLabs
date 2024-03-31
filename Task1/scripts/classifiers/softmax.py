5df5107a30223b314ee61497fbca62c0c5476271
Fomin <dortan_mors@mail.ru>
2024-03-31 14:10:42 +0400

    task1 3.1-3.6 subtasks

--- Task1/scripts/classifiers/softmax.py
+++ Task1/scripts/classifiers/softmax.py
@@ -21,8 +21,11 @@ def softmax_loss_naive(W, X, y, reg):
     - gradient with respect to weights W; an array of same shape as W
     """
     # Initialize the loss and gradient to zero.
+
+    D, C = W.shape
+    N = y.shape[0]
     loss = 0.0
-    dW = np.zeros_like(W)
+    dW = np.zeros_like(W.T)
 
     #############################################################################
     # TODO: Compute the softmax loss and its gradient using explicit loops.     #
@@ -32,11 +35,20 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
-
+    for n in range(N):
+        loss -= np.log(np.exp(W.T[y[n]].dot(X[n]))/(np.exp(W.T.dot(X[n]))).sum()) - np.linalg.norm(W) * reg
+    loss /= N
+    # for c in range(C):
+    #   for n in range(N):  
+    #       dW[c] -= X[n].reshape((-1,1)).dot(((np.arange(C) == c) - np.exp(W.T[c].dot(X[n]))/(np.exp(W.T.dot(X[n]))).sum()).reshape((-1,1)).T)
+    for c in range(C):
+      for n in range(N):  
+          dW[c] -= X[n]*((y[n] == c) - np.exp(W.T[c].dot(X[n]))/(np.exp(W.T.dot(X[n]))).sum()) - 2*W[:,c]*reg
+    dW /= N
+    
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    return loss, dW
+    return loss, dW.T
 
 
 def softmax_loss_vectorized(W, X, y, reg):
@@ -48,7 +60,8 @@ def softmax_loss_vectorized(W, X, y, reg):
     # Initialize the loss and gradient to zero.
     loss = 0.0
     dW = np.zeros_like(W)
-
+    D, C = W.shape
+    N = y.shape[0]
     #############################################################################
     # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
     # Store the loss in loss and the gradient in dW. If you are not careful     #
@@ -56,9 +69,12 @@ def softmax_loss_vectorized(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    
+    loss -= (np.log(np.exp((X * W[:,y].T).sum(-1))/(np.exp((W.T.dot(X.T).T)).sum(-1)))).sum() - N * np.linalg.norm(W) * reg
+    loss /= N
 
-    pass
-
+    dW = -X.T.dot(((np.logical_not(np.equal(np.full((N,C),np.arange(C)).T,np.full((C,N),y)))) - (np.exp((X * W[:,y].T).sum(-1))/(np.exp((W.T.dot(X.T).T)).sum(-1)))).T) - 2*W*reg*N
+    dW /= N
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     return loss, dW
